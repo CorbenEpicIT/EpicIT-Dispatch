10e258e8f65a4d88641888bd06fd1e40dd3ed89b
max <max@epicitautomations.com>
2026-07-11 12:31:45 -0500

    fix: 🐛 dropped duplicate vehicle rename from mfa_totp migration

--- backend/prisma/migrations/20260708224442_mfa_totp/migration.sql
+++ backend/prisma/migrations/20260708224442_mfa_totp/migration.sql
@@ -1,12 +1,6 @@
 -- AlterTable
 ALTER TABLE "organization" ADD COLUMN     "mfa_required" BOOLEAN NOT NULL DEFAULT false;
 
--- AlterTable
-ALTER TABLE "vehicle_restock_line" RENAME CONSTRAINT "vehicle_eod_restock_line_pkey" TO "vehicle_restock_line_pkey";
-
--- AlterTable
-ALTER TABLE "vehicle_restock_record" RENAME CONSTRAINT "vehicle_eod_record_pkey" TO "vehicle_restock_record_pkey";
-
 -- CreateTable
 CREATE TABLE "mfa_credential" (
     "id" TEXT NOT NULL,
@@ -39,45 +33,3 @@ CREATE UNIQUE INDEX "mfa_credential_user_id_role_key" ON "mfa_credential"("user_
 
 -- CreateIndex
 CREATE INDEX "mfa_recovery_code_user_id_role_idx" ON "mfa_recovery_code"("user_id", "role");
-
--- RenameForeignKey
-ALTER TABLE "stock_movement" RENAME CONSTRAINT "stock_movement_eod_record_id_fkey" TO "stock_movement_restock_record_id_fkey";
-
--- RenameForeignKey
-ALTER TABLE "vehicle_readiness" RENAME CONSTRAINT "vehicle_readiness_eod_record_id_fkey" TO "vehicle_readiness_restock_record_id_fkey";
-
--- RenameForeignKey
-ALTER TABLE "vehicle_restock_line" RENAME CONSTRAINT "vehicle_eod_restock_line_eod_record_id_fkey" TO "vehicle_restock_line_restock_record_id_fkey";
-
--- RenameForeignKey
-ALTER TABLE "vehicle_restock_line" RENAME CONSTRAINT "vehicle_eod_restock_line_stock_item_id_fkey" TO "vehicle_restock_line_stock_item_id_fkey";
-
--- RenameForeignKey
-ALTER TABLE "vehicle_restock_record" RENAME CONSTRAINT "vehicle_eod_record_completed_by_id_fkey" TO "vehicle_restock_record_completed_by_id_fkey";
-
--- RenameForeignKey
-ALTER TABLE "vehicle_restock_record" RENAME CONSTRAINT "vehicle_eod_record_completed_by_tech_id_fkey" TO "vehicle_restock_record_completed_by_tech_id_fkey";
-
--- RenameForeignKey
-ALTER TABLE "vehicle_restock_record" RENAME CONSTRAINT "vehicle_eod_record_vehicle_id_fkey" TO "vehicle_restock_record_vehicle_id_fkey";
-
--- RenameIndex
-ALTER INDEX "stock_movement_eod_record_id_idx" RENAME TO "stock_movement_restock_record_id_idx";
-
--- RenameIndex
-ALTER INDEX "vehicle_readiness_eod_record_id_key" RENAME TO "vehicle_readiness_restock_record_id_key";
-
--- RenameIndex
-ALTER INDEX "vehicle_eod_restock_line_eod_record_id_idx" RENAME TO "vehicle_restock_line_restock_record_id_idx";
-
--- RenameIndex
-ALTER INDEX "vehicle_eod_restock_line_stock_item_id_idx" RENAME TO "vehicle_restock_line_stock_item_id_idx";
-
--- RenameIndex
-ALTER INDEX "vehicle_eod_record_completed_at_idx" RENAME TO "vehicle_restock_record_completed_at_idx";
-
--- RenameIndex
-ALTER INDEX "vehicle_eod_record_organization_id_idx" RENAME TO "vehicle_restock_record_organization_id_idx";
-
--- RenameIndex
-ALTER INDEX "vehicle_eod_record_vehicle_id_idx" RENAME TO "vehicle_restock_record_vehicle_id_idx";
